4b779c351babf23b1c44ff8d86fd82b4a7e05818
Nikhil Badole <nikhilbadole4@gmail.com>
2023-04-17 01:54:39 +0530

    First

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,35 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private int basePrice;
+
+    private int vegBasePrice = 300;
+    private int nonVegBasePrice = 400;
+    private int extraCheese = 80;
+    private int extraToppingVeg = 70;
+    private int extraToppingNonVeg = 120;
+    private int paperBgPrice = 20;
+    private boolean isExtraCheese;
+    private boolean isTopping;
+    private boolean isTakeAway;
+
+
+
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        this.basePrice = getBasePrice();
+        this.price = basePrice;
+        this.isExtraCheese = false;
+        this.isTopping = false;
+        this.isTakeAway = false;
+    }
+
+    private int getBasePrice(){
+        if(isVeg) return vegBasePrice;
+        return nonVegBasePrice;
     }
 
     public int getPrice(){
@@ -17,18 +42,56 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!isExtraCheese){
+            isExtraCheese = true;
+            price+= extraCheese;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!isTopping){
+            isTopping = true;
+            if(isVeg){
+                price+= extraToppingVeg;
+            }else{
+                price+= extraToppingNonVeg;
+            }
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!isTakeAway){
+            isTakeAway = true;
+            price += paperBgPrice;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        String temp = "";
+        temp+= "Veg pizza base price:" + basePrice + "\n";
+        if(isExtraCheese){
+            temp+= "Extra Cheese Added:" + extraCheese + "\n";
+        }
+
+        if(isTopping){
+            if(isVeg){
+                temp+= "Extra Toppings Added:"+ extraToppingVeg + "\n";
+            }else{
+                temp+= "Extra Toppings Added:"+ nonVegBasePrice + "\n";
+            }
+        }
+
+        if(isTakeAway){
+            temp+= "Paperbag Added:" + paperBgPrice + "\n";
+        }
+
+        temp+= "Total Price:" + price +"\n";
+
+        this.bill = temp;
+
         return this.bill;
     }
 }
